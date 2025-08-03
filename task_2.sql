481901fb115ba85eb81de72be97c404e7b28f687
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-08-03 17:46:54 +0100

    2

--- task_2.sql
+++ task_2.sql
@@ -25,13 +25,13 @@ CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
