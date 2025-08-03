93d68f444d459337004fb66567ccf2a7cac28ffe
Chikizy06 <Chijinduonyemelukwe001@gmail.com>
2025-08-03 17:43:45 +0100

    3

---
+++ task_2.sql
@@ -0,0 +1,37 @@
+USE alx_book_store
+
+CREATE TABLE IF NOT EXISTS Books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors (author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR UNIQUE,
+    address TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details(
+    orderdetailid INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders (book_id),
+    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+);
